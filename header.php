961eb2e967582e2ca230e79ea3c8c4811b27df3f
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-30 20:02:46 +1300

    finished set up

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Embrace Design</title>
+</head>
+<body>
+
+    <?php if(has_nav_menu( 'top_navigation' )): ?>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'    => 'top_navigation',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'top_navigation',
+                    'menu_class'        => 'nav navbar-nav',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ) );
+                ?>
+            </div>
+            <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center">
+                <input type="hidden" name="post_type" value="post, movie"></input>
+                <input name="s" type="text" class="form-control" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+    <?php endif; ?>
--- index.php
+++ index.php
@@ -2,20 +2,29 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Embrace Design</title>
+        <title></title>
         <?php wp_head(); ?>
     </head>
     <body>
 
-        <?php if( have_posts() ): ?>
-            <?php while( have_posts() ): the_post(); ?>
-                <h1><?php the_title(); ?></h1>
-                <div>
-                    <?php the_content(); ?>
-                </div>
-            <?php endwhile; ?>
-        <?php endif; ?>
+        <div class="container">
+            <?php if( have_posts() ): ?>
+                <?php while( have_posts() ): the_post(); ?>
+                    <div class="card mt-3">
+                        <h5 class="card-header"><?php the_title(); ?></h5>
+                          <div class="card-body">
+                              <h5 class="card-title">Special Title Treatment</h5>
+                              <div>
+                                  <?php the_content(); ?>
+                              </div>
+                              <a href="#" class="btn btn-primary">Go Somewhere</a>
+                          </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
 
         <?php wp_footer(); ?>
+
     </body>
 </html>
