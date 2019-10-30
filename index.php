babbc596845bf87ca580acb32f1a03121121d5c0
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-30 21:30:38 +1300

    layout and design for the index page

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="card mt-3">
-        <h5 class="card-header"><?php the_title(); ?></h5>
-        <div class="card-body">
-            <div>
-                stuff will go here
-            </div>
-        </div>
-    </div>
+
 </div>
 
 <?php get_footer(); ?>
