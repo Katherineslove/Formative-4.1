28d8dd912adf39b796676fb3fd0d3186f2467adc
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-11-08 11:10:26 +1300

    changed the post format for the meta box

--- inc/productsInfoForm.php
+++ inc/productsInfoForm.php
@@ -1,4 +1,4 @@
 <div class="">
-    <label for="">Product Information</label>
-    <input type="text" name="F4_Products" value="<?php echo get_post_meta(get_the_ID(), 'F4_Products', true) ?>">
+    <label for="">Product Price</label>
+    <input type="number" min="0" max="9999" name="F4_Products" value="<?php echo get_post_meta(get_the_ID(), 'F4_Products', true) ?>">
 </div>
